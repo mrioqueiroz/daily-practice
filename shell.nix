acbf33b0eff90752f4db33034c77de1865a0c38a
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-03-15 23:33:46 -0300

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/84f800ef2421fee0cc2c83004b3e057e9cd78f2d.tar.gz")
+  { } }:
+let
+  dev-dependencies = with pkgs; [
+    rustup
+    tshark
+  ];
+in pkgs.mkShell {
+  name = "rust-practice";
+  buildInputs = [ dev-dependencies ];
+  shellHook = ''
+  '';
+  NIX_ENFORCE_PURITY = 0;
+}
