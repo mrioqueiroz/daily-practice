5bcf19cb0c9c28e3cae95485b6c2d57a5d284b12
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-03-29 23:13:59 -0300

    Minor updates

--- trust/src/main.rs
+++ trust/src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() -> io::Result<()> {
-    // Create a new interface.
+    // Create a new interface (user space network)
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = vec![0u8; 1504];
     loop {
