4942c9161735c66a84530bee3935ac5f7ecc44f8
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-05 23:14:27 -0300

    Define states and add tests

--- regex/src/main.rs
+++ regex/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
+enum State {
+    Locked,
+    Unlocked,
 }
+
+#[allow(dead_code)]
+enum Event {
+    Push,
+    Coin,
+}
+
+#[allow(dead_code)]
+fn next_state(state: State, event: Event) -> State {
+    match state {
+        State::Locked => match event {
+            Event::Push => State::Locked,
+            Event::Coin => State::Unlocked,
+        },
+        State::Unlocked => match event {
+            Event::Push => State::Locked,
+            Event::Coin => State::Unlocked,
+        },
+    }
+}
+
+#[test]
+fn on_push_if_locked_stay_locked() {
+    assert_eq!(next_state(State::Locked, Event::Push), State::Locked);
+}
+
+#[test]
+fn on_push_if_unlocked_move_to_locked() {
+    assert_eq!(next_state(State::Unlocked, Event::Push), State::Locked);
+}
+
+#[test]
+fn on_coin_if_locked_move_to_unlocked() {
+    assert_eq!(next_state(State::Locked, Event::Coin), State::Unlocked);
+}
+
+#[test]
+fn on_coin_if_unlocked_stay_unlocked() {
+    assert_eq!(next_state(State::Unlocked, Event::Coin), State::Unlocked);
+}
+
+fn main() {}
