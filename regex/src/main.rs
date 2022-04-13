e0b3ff4c91b96a1702706858dfad0a766c69670a
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-12 22:01:13 -0300

    Implement compile and match

--- regex/src/main.rs
+++ regex/src/main.rs
@@ -3,15 +3,48 @@ use std::ops::Range;
 const FSM_COLUMN_SIZE: usize = 130;
 const FSM_NEW_LINE: usize = FSM_COLUMN_SIZE - 1;
 
-struct Fsm {
+struct Regex {
     columns: Vec<FsmColumn>,
 }
 
-impl Fsm {
-    fn new() -> Self {
-        Self {
+impl Regex {
+    fn compile(src: &str) -> Self {
+        let mut fsm = Self {
             columns: Vec::new(),
+        };
+        fsm.push(FsmColumn::new());
+
+        for c in src.chars() {
+            let mut col = FsmColumn::new();
+            match c {
+                '$' => {
+                    col.transition[FSM_NEW_LINE] = fsm.columns.len() + 1;
+                }
+                _ => {
+                    col.transition[c as usize] = fsm.columns.len() + 1;
+                }
+            }
+            fsm.columns.push(col)
         }
+        fsm
+    }
+
+    fn match_str(&self, input: &str) -> bool {
+        // Successful state.
+        let mut state = 1;
+        for c in input.chars() {
+            if state == 0 || state >= self.columns.len() {
+                break;
+            }
+            state = self.columns[state].transition[c as usize];
+        }
+        if state == 0 {
+            return false;
+        }
+        if state < self.columns.len() {
+            state = self.columns[state].transition[FSM_NEW_LINE]
+        }
+        state >= self.columns.len()
     }
 
     fn push(&mut self, column: FsmColumn) {
@@ -43,6 +76,7 @@ impl FsmColumn {
         }
     }
 
+    #[allow(dead_code)]
     fn fill_range(&mut self, range: Range<char>, state: FsmIndex) {
         for i in range {
             self.transition[i as usize] = state;
@@ -50,43 +84,13 @@ impl FsmColumn {
     }
 }
 
-fn match_fsm(fsm: &Fsm, input: &str) -> bool {
-    // Successful state.
-    let mut state = 1;
-    for c in input.chars() {
-        if state == 0 || state >= fsm.columns.len() {
-            break;
-        }
-        state = fsm.columns[state].transition[c as usize];
-    }
-    if state == 0 {
-        return false;
-    }
-    if state < fsm.columns.len() {
-        state = fsm.columns[state].transition[FSM_NEW_LINE]
-    }
-    return state >= fsm.columns.len();
-}
-
 fn main() {
-    let mut fsm = Fsm::new();
+    let regex = Regex::compile("abcdef$");
 
-    let events = vec!['a' as usize, 'b' as usize, 'c' as usize, FSM_NEW_LINE];
-
-    // Failed state
-    fsm.push(FsmColumn::new());
-
-    for event in events {
-        let mut col = FsmColumn::new();
-        // Keep track of the state
-        col.transition[event] = fsm.columns.len() + 1;
-        fsm.push(col);
-    }
-
-    fsm.dump();
+    regex.dump();
 
     let inputs = vec!["Hello, World", "abc", "abcd"];
     for input in inputs.iter() {
-        println!("{:?} => {:?}", input, match_fsm(&fsm, input));
+        println!("{:?} => {:?}", input, regex.match_str(input));
     }
 }
