fddd43fe38e606d1da8a1b17c9e63fe7fbd03524
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-21 23:38:49 -0300

    Start API functionality

---
+++ tdapi/src/main.rs
@@ -0,0 +1,18 @@
+use std::{thread::sleep, time::Duration};
+
+const CLEAR: &str = "\x1B[2J\x1B[1;1H";
+
+fn main() {
+    let v = vec![1, 2, 3];
+    let mut progress_state = 1;
+
+    for n in v.iter() {
+        println!("{}{}", CLEAR, "*".repeat(progress_state));
+        progress_state += 1;
+        expensive_calculation(n);
+    }
+}
+
+fn expensive_calculation(_n: &i32) {
+    sleep(Duration::from_secs(1));
+}
