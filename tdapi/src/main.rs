aeb57e606d7bf98a6be1d9eb39ff28ac86ceca3e
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-21 23:44:24 -0300

    Work on any vector

--- tdapi/src/main.rs
+++ tdapi/src/main.rs
@@ -4,6 +4,10 @@ const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
 fn main() {
     let v = vec![1, 2, 3];
+    progress(v);
+}
+
+fn progress(v: Vec<i32>) {
     let mut progress_state = 1;
 
     for n in v.iter() {
