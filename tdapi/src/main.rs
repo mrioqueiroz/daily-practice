5ca52fb3a871a0c0767eeb5b438d117bbf5ff4a9
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-21 23:52:04 -0300

    Use function as parameter for progress

--- tdapi/src/main.rs
+++ tdapi/src/main.rs
@@ -4,19 +4,19 @@ const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v);
+    progress(v, expensive_calculation);
 }
 
-fn progress<T>(v: Vec<T>) {
+fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
     let mut progress_state = 1;
 
     for n in v.iter() {
         println!("{}{}", CLEAR, "*".repeat(progress_state));
         progress_state += 1;
-        expensive_calculation(n);
+        f(n);
     }
 }
 
-fn expensive_calculation<T>(_n: T) {
+fn expensive_calculation<T>(_n: &T) {
     sleep(Duration::from_secs(1));
 }
