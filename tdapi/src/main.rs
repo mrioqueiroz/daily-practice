33e9276f7ff0702a008916851f7951ce2c587a33
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-23 01:22:00 -0300

    Implement representation with bounds when size is known

--- tdapi/src/main.rs
+++ tdapi/src/main.rs
@@ -5,6 +5,7 @@ const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 struct Progress<Iter> {
     iter: Iter,
     progress_state: usize,
+    bound: Option<usize>,
 }
 
 // For all types Iter, implement Progress of Iter.
@@ -14,7 +15,26 @@ impl<Iter> Progress<Iter> {
         Self {
             iter,
             progress_state: 0,
+            bound: None,
         }
+    }
+}
+
+// According to signature, ExactSizeIterator requires that anything that
+// implements this trait also implement Iterator. So, ExactSizeIterator is an
+// iterator that ALSO has the two methods len() and is_empty(). Here, it
+// inherits only the specification, not the implementation (use
+// inheritance-style to express requirements).
+impl<Iter> Progress<Iter>
+where
+    Iter: ExactSizeIterator,
+{
+    // Add this method to the Progress data structure, but only where this type
+    // Iter implements the trait ExactSizeIterator.
+    #[allow(dead_code)]
+    fn with_bound(mut self) -> Self {
+        self.bound = Some(self.iter.len());
+        self
     }
 }
 
@@ -28,7 +48,16 @@ where
     // Item is whatever is returned from the inner iterator.
     type Item = Iter::Item;
     fn next(&mut self) -> Option<Self::Item> {
-        println!("{}{}", CLEAR, "*".repeat(self.progress_state));
+        print!("{}", CLEAR);
+        match self.bound {
+            // If we have the bound...
+            Some(bound) => println!(
+                "[{}{}]",
+                "*".repeat(self.progress_state),
+                " ".repeat(bound - self.progress_state)
+            ),
+            None => println!("{}", "*".repeat(self.progress_state)),
+        }
         self.progress_state += 1;
         self.iter.next()
     }
@@ -60,8 +89,14 @@ fn expensive_calculation<T>(_n: &T) {
 }
 
 fn main() {
+    // Bounded.
     let v = vec![1, 2, 3];
-    for n in v.iter().progress() {
+    for n in v.iter().progress().with_bound() {
         expensive_calculation(n);
+    }
+
+    // Not bounded.
+    for n in (0..).progress() {
+        expensive_calculation(&n);
     }
 }
