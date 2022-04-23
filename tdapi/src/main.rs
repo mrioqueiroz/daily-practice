10f0ef0768f2d37aa4fb825804903d26f1399854
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-23 00:43:01 -0300

    Implement Iterator for Progress

--- tdapi/src/main.rs
+++ tdapi/src/main.rs
@@ -1,30 +1,46 @@
-use std::collections::HashSet;
 use std::{thread::sleep, time::Duration};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-fn main() {
-    let v = vec![1, 2, 3];
-    progress(v.iter(), expensive_calculation);
-
-    let mut h = HashSet::new();
-    h.insert(0);
-    progress(h.iter(), expensive_calculation);
+struct Progress<Iter> {
+    iter: Iter,
+    progress_state: usize,
 }
 
-fn progress<Iter>(iter: Iter, f: fn(Iter::Item) -> ())
+// For all types Iter, implement Progress of Iter.
+impl<Iter> Progress<Iter> {
+    // Constructor,
+    fn new(iter: Iter) -> Self {
+        Self {
+            iter,
+            progress_state: 0,
+        }
+    }
+}
+
+// Make the compiler understand that the progress data structure is an iterator
+// and can be given to a for loop, by satisfying the requirements of the
+// Iterator interface.
+impl<Iter> Iterator for Progress<Iter>
 where
     Iter: Iterator,
 {
-    let mut progress_state = 1;
-
-    for n in iter {
-        println!("{}{}", CLEAR, "*".repeat(progress_state));
-        progress_state += 1;
-        f(n);
+    // Item is whatever is returned from the inner iterator.
+    type Item = Iter::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        println!("{}{}", CLEAR, "*".repeat(self.progress_state));
+        self.progress_state += 1;
+        self.iter.next()
     }
 }
 
 fn expensive_calculation<T>(_n: &T) {
     sleep(Duration::from_secs(1));
+}
+
+fn main() {
+    let v = vec![1, 2, 3];
+    for n in Progress::new(v.iter()) {
+        expensive_calculation(n);
+    }
 }
