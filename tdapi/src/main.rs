3a821a4293d27e2da504f8ca5bf6ac30d5def992
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-04-23 00:13:39 -0300

    Make function generic over all iterables

--- tdapi/src/main.rs
+++ tdapi/src/main.rs
@@ -1,16 +1,24 @@
+use std::collections::HashSet;
 use std::{thread::sleep, time::Duration};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v, expensive_calculation);
+    progress(v.iter(), expensive_calculation);
+
+    let mut h = HashSet::new();
+    h.insert(0);
+    progress(h.iter(), expensive_calculation);
 }
 
-fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
+fn progress<T, Iter>(iter: Iter, f: fn(T) -> ())
+where
+    Iter: Iterator<Item = T>,
+{
     let mut progress_state = 1;
 
-    for n in v.iter() {
+    for n in iter {
         println!("{}{}", CLEAR, "*".repeat(progress_state));
         progress_state += 1;
         f(n);
