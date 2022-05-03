d0ab5767136a14236eec9abde7a506868bfeaa9d
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-05-03 00:23:04 -0300

    Adjust names on shell

--- shell.nix
+++ shell.nix
@@ -2,14 +2,14 @@
   "https://github.com/NixOS/nixpkgs/archive/84f800ef2421fee0cc2c83004b3e057e9cd78f2d.tar.gz")
   { } }:
 let
-  dev-dependencies = with pkgs; [
+  rust-dependencies = with pkgs; [
     ncurses
     rustup
     tshark
   ];
 in pkgs.mkShell {
-  name = "rust-practice";
-  buildInputs = [ dev-dependencies ];
+  name = "daily-practice";
+  buildInputs = [ rust-dependencies ];
   shellHook = ''
   '';
   NIX_ENFORCE_PURITY = 0;
