5605e514691fc7b880e614af2a96a362a83f6aa1
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-05-12 02:26:40 -0300

    Define AST and Parser and parse null

---
+++ hsjson/Main.hs
@@ -0,0 +1,57 @@
+module Main where
+
+import Data.Char
+
+-- Define AST.
+data JsonValue
+  = JsonNull
+  | JsonBool Bool -- Encapsulate the boolean type of Haskell.
+  | JsonNumber Integer -- No support for floats.
+  | JsonString String
+  | JsonArray [JsonValue] -- The type becomes recursive.
+  | JsonObject [(String, JsonValue)] -- Associative array.
+  deriving (Show, Eq)
+
+-- Define parser.
+-- NOTE: No proper error reporting.
+-- Is parameterized by the thing it parses.
+newtype Parser a = Parser
+  -- runParser is a field, but it generates a function.
+  { runParser :: String -> Maybe (String, a)
+  }
+
+-- Prove that the Parser is a Functor.
+instance Functor Parser where
+  fmap f (Parser p) = Parser $ \input -> do
+    (input', x) <- p input
+    Just (input', f x)
+
+-- Prove that the Parser is a Applicative.
+instance Applicative Parser where
+  pure x = Parser $ \input -> Just (input, x)
+  (Parser p1) <*> (Parser p2) = Parser $ \input -> do
+    (input', f) <- p1 input
+    (input'', a) <- p2 input'
+    Just (input'', f a)
+
+-- Parse a single character.
+charP :: Char -> Parser Char
+charP x = Parser f
+  where
+    f (y : ys)
+      | y == x = Just (ys, x)
+      | otherwise = Nothing
+    f [] = Nothing
+
+stringP :: String -> Parser String
+stringP = sequenceA . map charP
+
+-- Just need to parse a sequence of characters ("null").
+jsonNull :: Parser JsonValue
+jsonNull = (\_ -> JsonNull) <$> stringP "null"
+
+jsonValue :: Parser JsonValue
+jsonValue = undefined
+
+main :: IO ()
+main = undefined
