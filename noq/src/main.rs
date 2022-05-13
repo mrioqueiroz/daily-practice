c67398ca23ee1e897332e6cb97bd4b67ee7317f6
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-05-13 02:30:12 -0300

    Add pattern matching

--- noq/src/main.rs
+++ noq/src/main.rs
@@ -1,7 +1,7 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 #[allow(dead_code)]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Expr {
     Sym(String),
     // Functor (self-referential type)
@@ -46,6 +46,40 @@ impl Rule {
     }
 }
 
+type Bindings = HashMap<String, Expr>;
+
+fn pattern_match(pattern: &Expr, value: &Expr) -> Option<Bindings> {
+    fn pattern_match_impl(pattern: &Expr, value: &Expr, bindings: &mut Bindings) -> bool {
+        use Expr::*;
+        match (pattern, value) {
+            (Sym(name), _) => {
+                bindings.insert(name.clone(), value.clone());
+                true
+            }
+            (Fun(name1, args1), Fun(name2, args2)) => {
+                if name1 == name2 && args1.len() == args2.len() {
+                    for i in 0..args1.len() {
+                        if !pattern_match_impl(&args1[i], &args2[i], bindings) {
+                            return false;
+                        }
+                    }
+                    true
+                } else {
+                    false
+                }
+            }
+            (Fun(_, _), Sym(_)) => todo!(),
+        }
+    }
+
+    let mut bindings = HashMap::new();
+    if pattern_match_impl(&pattern, &value, &mut bindings) {
+        Some(bindings)
+    } else {
+        None
+    }
+}
+
 fn main() {
     use Expr::*;
     // swap(pair(a, b)) = pair(b, a)
@@ -62,5 +96,23 @@ fn main() {
             vec![Sym("b".to_string()), Sym("a".to_string())],
         ),
     };
-    println!("{}", swap);
+
+    // swap(pair(a, b))
+    let pattern = swap.head;
+
+    // swap(pair(f(c), g(d)))
+    let value = Fun(
+        "swap".to_string(),
+        vec![Fun(
+            "pair".to_string(),
+            vec![
+                Fun("f".to_string(), vec![Sym("c".to_string())]),
+                Fun("g".to_string(), vec![Sym("d".to_string())]),
+            ],
+        )],
+    );
+
+    println!("Pattern: {}", pattern);
+    println!("Value: {}", value);
+    println!("{:?}", pattern_match(&pattern, &value));
 }
