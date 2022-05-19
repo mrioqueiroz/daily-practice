85afcc780b2b0ae1e80b03f06edf97926aac65be
Mário Queiroz <mrioqueiroz@protonmail.com>
2022-05-19 02:54:32 -0300

    Fix solution

--- cola/main.c
+++ cola/main.c
@@ -1,23 +1,17 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
 
 const char *
 who_is_next (long long n, size_t length, const char *const names[length])
 {
-  long long *sizes = malloc(length * sizeof(n));
-
-  for (size_t i = 0; i < length; ++i) {
-    sizes[i] = 1LL;
+  long long sizes = 1;
+  while (n > sizes * length) {
+    n -= sizes * length;
+    sizes *= 2;
   }
-
-  size_t head = 0;
-
-  while (n > sizes[head]) {
-    n -= sizes[head];
-    sizes[head] *= 2;
-    head = (head + 1) % length;
-  }
-  return names[head];
+  assert(n <= sizes * length);
+  return names[(n - 1) / sizes];
 }
 
 int main(void) {
